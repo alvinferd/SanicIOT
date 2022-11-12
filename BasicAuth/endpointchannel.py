36821f91798ef9b3c1613f25f620f987f5115dcb
alvinferd <alvin_ferd@apps.ipb.ac.id>
2022-11-12 09:26:31 +0700

    indent async pool

--- BasicAuth/endpointchannel.py
+++ BasicAuth/endpointchannel.py
@@ -26,35 +26,35 @@ async def addchannel(request):
                     return response.json({"description": "Forbidden",'status': 403, 'message': "Wrong Password."}, status=403)
             else:
                 return response.json({"description": "Forbidden",'status': 403, 'message': "Username not found"}, status=403)
-            id_user = iduser
-            data = request.json
-            pool = request.app.config['pool']
-            async with pool.acquire() as conn:
-                try:
-                    if (data["value"] and str(data["id_sensor"]).isdigit()):
-                        sql = '''
-                                SELECT node.id_user from node Inner join sensor on node.id_node = sensor.id_node 
-                                WHERE sensor.id_sensor = {0}; 
-                            '''.format(str(data["id_sensor"]))
-                        try:
-                            rows = await conn.fetch(sql)
-                            iduser = jsonify(rows)[0]["id_user"]
-                            if(id_user == iduser or authtoken["is_admin"] == 1):
-                                valid =1
-                            else:
-                                return response.json({"description": "Forbidden",'status': 403, "message": "You can't send channel to another user's sensor"}, status=403)                                                              
-                        except:
-                            return response.json({"description": "Bad Request",'status': 400, "message": "Id sensor not found"}, status=400)                                                                   
-                    else:
-                        return response.json({"description": "Bad Request",'status': 400, "message": "Empty request body"}, status=400)         
-                except:
-                    return response.json({"description": "Bad Request",'status': 400, "message": "Missing parameter"}, status=400)
-                sql = """
-                    INSERT into channel (time, value, id_sensor) values
-                    (current_timestamp,'{0}',{1});
-                """.format(str(data["value"]), str(data["id_sensor"]))
-                rows = await conn.execute(sql)
-                return response.json({"description": "Created",'status': 201, 'message':'Successfully add new channel'}, status=201)        
+        id_user = iduser
+        data = request.json
+        pool = request.app.config['pool']
+        async with pool.acquire() as conn:
+            try:
+                if (data["value"] and str(data["id_sensor"]).isdigit()):
+                    sql = '''
+                            SELECT node.id_user from node Inner join sensor on node.id_node = sensor.id_node 
+                            WHERE sensor.id_sensor = {0}; 
+                        '''.format(str(data["id_sensor"]))
+                    try:
+                        rows = await conn.fetch(sql)
+                        iduser = jsonify(rows)[0]["id_user"]
+                        if(id_user == iduser or authtoken["is_admin"] == 1):
+                            valid =1
+                        else:
+                            return response.json({"description": "Forbidden",'status': 403, "message": "You can't send channel to another user's sensor"}, status=403)                                                              
+                    except:
+                        return response.json({"description": "Bad Request",'status': 400, "message": "Id sensor not found"}, status=400)                                                                   
+                else:
+                    return response.json({"description": "Bad Request",'status': 400, "message": "Empty request body"}, status=400)         
+            except:
+                return response.json({"description": "Bad Request",'status': 400, "message": "Missing parameter"}, status=400)
+            sql = """
+                INSERT into channel (time, value, id_sensor) values
+                (current_timestamp,'{0}',{1});
+            """.format(str(data["value"]), str(data["id_sensor"]))
+            rows = await conn.execute(sql)
+            return response.json({"description": "Created",'status': 201, 'message':'Successfully add new channel'}, status=201)        
 #    else:
 #        return response.json({"description": "Forbidden",'status': 403, 'message': "You are unauthorized, invalid token."}, status=403)
 
--- BasicAuth/endpointnode.py
+++ BasicAuth/endpointnode.py
@@ -25,33 +25,33 @@ async def addnode(request):
                     return response.json({"description": "Forbidden",'status': 403, 'message': "Wrong Password."}, status=403)
             else:
                 return response.json({"description": "Forbidden",'status': 403, 'message': "Username not found"}, status=403)
-            data = request.json 
-            pool = request.app.config['pool']
-            async with pool.acquire() as conn:
-                try:
-                    if (data["name"] and data["location"] and str(data["id_hardware"]).isdigit()):
-                        sql = """
-                            SELECT hardware.type from hardware WHERE id_hardware = {0};
-                            """.format(str(data["id_hardware"]))
-                        rows = await conn.fetch(sql)
-                        if(bool(rows)):
-                            res_hardware = jsonify(rows)[0]
-                            if(res_hardware["type"] == "single-board computer" or res_hardware["type"] == "microcontroller unit"):
-                                valid = 1
-                            else :
-                                return response.json({"description": "Bad Request",'status': 400, "message": "Hardware type not match, type should be single-board computer or microcontroller unit"}, status=400)                               
-                        else:   
-                            return response.json({"description": "Bad Request",'status': 400, "message": "Id hardware not found"}, status=400)                           
-                    else:
-                        return response.json({"description": "Bad Request",'status': 400, "message": "Empty request body"}, status=400)         
-                except:
-                    return response.json({"description": "Bad Request",'status': 400, "message": "Missing parameter"}, status=400)
-                id_user = iduser
-                sql = """
-                    INSERT into node (name, location, id_hardware,id_user) values ('{0}','{1}',{2},{3})
-                """.format(data["name"], data["location"], str(data["id_hardware"]), str(id_user))
-                rows = await conn.execute(sql)
-                return response.json({"description": "Created",'status': 201, 'message':'Successfully add new node'}, status=201)        
+        data = request.json 
+        pool = request.app.config['pool']
+        async with pool.acquire() as conn:
+            try:
+                if (data["name"] and data["location"] and str(data["id_hardware"]).isdigit()):
+                    sql = """
+                        SELECT hardware.type from hardware WHERE id_hardware = {0};
+                        """.format(str(data["id_hardware"]))
+                    rows = await conn.fetch(sql)
+                    if(bool(rows)):
+                        res_hardware = jsonify(rows)[0]
+                        if(res_hardware["type"] == "single-board computer" or res_hardware["type"] == "microcontroller unit"):
+                            valid = 1
+                        else :
+                            return response.json({"description": "Bad Request",'status': 400, "message": "Hardware type not match, type should be single-board computer or microcontroller unit"}, status=400)                               
+                    else:   
+                        return response.json({"description": "Bad Request",'status': 400, "message": "Id hardware not found"}, status=400)                           
+                else:
+                    return response.json({"description": "Bad Request",'status': 400, "message": "Empty request body"}, status=400)         
+            except:
+                return response.json({"description": "Bad Request",'status': 400, "message": "Missing parameter"}, status=400)
+            id_user = iduser
+            sql = """
+                INSERT into node (name, location, id_hardware,id_user) values ('{0}','{1}',{2},{3})
+            """.format(data["name"], data["location"], str(data["id_hardware"]), str(id_user))
+            rows = await conn.execute(sql)
+            return response.json({"description": "Created",'status': 201, 'message':'Successfully add new node'}, status=201)        
 #    else:
 #        return response.json({"description": "Forbidden",'status': 403, 'message': "You are unauthorized, invalid token."}, status=403)
 
@@ -108,38 +108,38 @@ async def detailnode(request, id_node: int):
                     return response.json({"description": "Forbidden",'status': 403, 'message': "Wrong Password."}, status=403)
             else:
                 return response.json({"description": "Forbidden",'status': 403, 'message': "Username not found"}, status=403)
-            id_user = iduser
-            pool = request.app.config['pool']
-            async with pool.acquire() as conn:
-                sql = '''
-                        SELECT * from node where id_node = {0}; 
-                    '''.format(id_node)
-                rows = await conn.fetch(sql)
-                if(bool(rows)):
-                    res = jsonify(rows)[0]
-                    iduser = res["id_user"]
-                    if(id_user == iduser or authtoken["is_admin"] == 1):
-                        rowsFinal = res
-                        sql = '''
-                                SELECT hardware.name, hardware.type from hardware 
-                                left join node on hardware.id_hardware = node.id_hardware 
-                                where id_node = {0}; 
-                            '''.format(id_node)
-                        rowsFinal["hardware"] = jsonify(await conn.fetch(sql))
-                        sql = '''
-                                SELECT sensor.id_sensor, sensor.name, sensor.unit from sensor
-                                left join node on sensor.id_node = node.id_node
-                                where sensor.id_node = {0}; 
-                            '''.format(id_node)
-                        try:
-                            rowsFinal["sensor"] = jsonify(await conn.fetch(sql))
-                        except:
-                            rowsFinal["sensor"] = "{}"
-                        return response.json({'status': 200, 'data': rowsFinal}, status=200)
-                    else:
-                        return response.json({"description": "Forbidden",'status': 403, "message": "You can't see other user's node"}, status=403)                                                              
+        id_user = iduser
+        pool = request.app.config['pool']
+        async with pool.acquire() as conn:
+            sql = '''
+                    SELECT * from node where id_node = {0}; 
+                '''.format(id_node)
+            rows = await conn.fetch(sql)
+            if(bool(rows)):
+                res = jsonify(rows)[0]
+                iduser = res["id_user"]
+                if(id_user == iduser or authtoken["is_admin"] == 1):
+                    rowsFinal = res
+                    sql = '''
+                            SELECT hardware.name, hardware.type from hardware 
+                            left join node on hardware.id_hardware = node.id_hardware 
+                            where id_node = {0}; 
+                        '''.format(id_node)
+                    rowsFinal["hardware"] = jsonify(await conn.fetch(sql))
+                    sql = '''
+                            SELECT sensor.id_sensor, sensor.name, sensor.unit from sensor
+                            left join node on sensor.id_node = node.id_node
+                            where sensor.id_node = {0}; 
+                        '''.format(id_node)
+                    try:
+                        rowsFinal["sensor"] = jsonify(await conn.fetch(sql))
+                    except:
+                        rowsFinal["sensor"] = "{}"
+                    return response.json({'status': 200, 'data': rowsFinal}, status=200)
                 else:
-                    return response.json({"description": "Bad Request",'status': 400, "message": "Id node not found"}, status=400)                                                              
+                    return response.json({"description": "Forbidden",'status': 403, "message": "You can't see other user's node"}, status=403)                                                              
+            else:
+                return response.json({"description": "Bad Request",'status': 400, "message": "Id node not found"}, status=400)                                                              
 #    else:
 #        return response.json({"description": "Forbidden",'status': 403, 'message': "You are unauthorized, invalid token."}, status=403)
 
@@ -165,46 +165,46 @@ async def editnode(request, id_node: int):
                     return response.json({"description": "Forbidden",'status': 403, 'message': "Wrong Password."}, status=403)
             else:
                 return response.json({"description": "Forbidden",'status': 403, 'message': "Username not found"}, status=403)
-            data = request.json 
-            id_user = iduser
-            pool = request.app.config['pool']
-            async with pool.acquire() as conn:
-                sql = '''
-                        SELECT id_user from node where id_node = {0}; 
-                    '''.format(id_node)
-                rows = await conn.fetch(sql)
-                if(bool(rows)):
-                    iduser = jsonify(rows)[0]["id_user"]
-                    if(id_user == iduser or authtoken["is_admin"] == 1):
-                        try:
-                            if (data["name"] and data["location"] and str(data["id_hardware"]).isdigit()):
-                                sql = """
-                                    SELECT hardware.type from hardware WHERE id_hardware = {0};
-                                    """.format(str(data["id_hardware"]))
-                                rows = await conn.fetch(sql)
-                                if(bool(rows)):
-                                    res_hardware = jsonify(rows)[0]
-                                    if(res_hardware["type"] == "single-board computer" or res_hardware["type"] == "microcontroller unit"):
-                                        valid = 1
-                                    else :
-                                        return response.json({"description": "Bad Request",'status': 400, "message": "Hardware type not match, type should be single-board computer or microcontroller unit"}, status=400)                               
-                                else:   
-                                    return response.json({"description": "Bad Request",'status': 400, "message": "Id hardware not found"}, status=400)                           
-                            else:
-                                return response.json({"description": "Bad Request",'status': 400, "message": "Empty request body"}, status=400)         
-                        except:
-                            return response.json({"description": "Bad Request",'status': 400, "message": "Missing parameter"}, status=400)
-                        sql = """
-                            UPDATE node
-                            SET name = '{0}', location = '{1}', id_hardware = {2}
-                            WHERE id_node = {3};
-                        """.format(data["name"], data["location"], str(data["id_hardware"]), str(id_node))
-                        rows = await conn.execute(sql)
-                        return response.json({"description": "Created",'status': 201, 'message':'Successfully edit node, ID : {0}'.format(id_node)}, status=201)                        
-                    else:
-                        return response.json({"description": "Forbidden",'status': 403, "message": "Can't edit other user's node"}, status=403)                                                              
+        data = request.json 
+        id_user = iduser
+        pool = request.app.config['pool']
+        async with pool.acquire() as conn:
+            sql = '''
+                    SELECT id_user from node where id_node = {0}; 
+                '''.format(id_node)
+            rows = await conn.fetch(sql)
+            if(bool(rows)):
+                iduser = jsonify(rows)[0]["id_user"]
+                if(id_user == iduser or authtoken["is_admin"] == 1):
+                    try:
+                        if (data["name"] and data["location"] and str(data["id_hardware"]).isdigit()):
+                            sql = """
+                                SELECT hardware.type from hardware WHERE id_hardware = {0};
+                                """.format(str(data["id_hardware"]))
+                            rows = await conn.fetch(sql)
+                            if(bool(rows)):
+                                res_hardware = jsonify(rows)[0]
+                                if(res_hardware["type"] == "single-board computer" or res_hardware["type"] == "microcontroller unit"):
+                                    valid = 1
+                                else :
+                                    return response.json({"description": "Bad Request",'status': 400, "message": "Hardware type not match, type should be single-board computer or microcontroller unit"}, status=400)                               
+                            else:   
+                                return response.json({"description": "Bad Request",'status': 400, "message": "Id hardware not found"}, status=400)                           
+                        else:
+                            return response.json({"description": "Bad Request",'status': 400, "message": "Empty request body"}, status=400)         
+                    except:
+                        return response.json({"description": "Bad Request",'status': 400, "message": "Missing parameter"}, status=400)
+                    sql = """
+                        UPDATE node
+                        SET name = '{0}', location = '{1}', id_hardware = {2}
+                        WHERE id_node = {3};
+                    """.format(data["name"], data["location"], str(data["id_hardware"]), str(id_node))
+                    rows = await conn.execute(sql)
+                    return response.json({"description": "Created",'status': 201, 'message':'Successfully edit node, ID : {0}'.format(id_node)}, status=201)                        
                 else:
-                    return response.json({"description": "Bad Request",'status': 400, "message": "Id node not found"}, status=400)                                                              
+                    return response.json({"description": "Forbidden",'status': 403, "message": "Can't edit other user's node"}, status=403)                                                              
+            else:
+                return response.json({"description": "Bad Request",'status': 400, "message": "Id node not found"}, status=400)                                                              
 #    else:
 #        return response.json({"description": "Forbidden",'status': 403, 'message': "You are unauthorized, invalid token."}, status=403)
 
@@ -230,28 +230,28 @@ async def deletenode(request, id_node: int):
                     return response.json({"description": "Forbidden",'status': 403, 'message': "Wrong Password."}, status=403)
             else:
                 return response.json({"description": "Forbidden",'status': 403, 'message': "Username not found"}, status=403)
-            id_user = iduser
-            pool = request.app.config['pool']
-            async with pool.acquire() as conn:
-                sql = '''
-                        SELECT id_user from node where id_node = {0}; 
-                    '''.format(id_node)
-                rows = await conn.fetch(sql)
-                if(bool(rows)):
-                    iduser = jsonify(rows)[0]["id_user"]
-                    if(id_user == iduser or authtoken["is_admin"] == 1):
-                        sql = '''
-                                DELETE from node where id_node = {0}; 
-                            '''.format(id_node)
-                        rows = await conn.execute(sql)
-                        if(rows=="DELETE 1"):
-                            return response.json({'status': 200, 'data': "Successfully delete node, id: {0}".format(str(id_node))}, status=200)
-                        else:
-                            return response.json({"description": "Bad Request",'status': 400, "message": "Id node not found"}, status=400)                                                                              
+        id_user = iduser
+        pool = request.app.config['pool']
+        async with pool.acquire() as conn:
+            sql = '''
+                    SELECT id_user from node where id_node = {0}; 
+                '''.format(id_node)
+            rows = await conn.fetch(sql)
+            if(bool(rows)):
+                iduser = jsonify(rows)[0]["id_user"]
+                if(id_user == iduser or authtoken["is_admin"] == 1):
+                    sql = '''
+                            DELETE from node where id_node = {0}; 
+                        '''.format(id_node)
+                    rows = await conn.execute(sql)
+                    if(rows=="DELETE 1"):
+                        return response.json({'status': 200, 'data': "Successfully delete node, id: {0}".format(str(id_node))}, status=200)
                     else:
-                        return response.json({"description": "Forbidden",'status': 403, "message": "Can't delete other user's node"}, status=403)                                                              
+                        return response.json({"description": "Bad Request",'status': 400, "message": "Id node not found"}, status=400)                                                                              
                 else:
-                    return response.json({"description": "Bad Request",'status': 400, "message": "Id node not found"}, status=400)                                                              
+                    return response.json({"description": "Forbidden",'status': 403, "message": "Can't delete other user's node"}, status=403)                                                              
+            else:
+                return response.json({"description": "Bad Request",'status': 400, "message": "Id node not found"}, status=400)                                                              
 #    else:
 #        return response.json({"description": "Forbidden",'status': 403, 'message': "You are unauthorized, invalid token."}, status=403)
 
